a2b75baae33a291cc522fe2fe0199db7d7d1ac35
KevMcK <kmcrunk@gmail.com>
2024-02-29 23:35:20 -0600

    test updates

--- TestScripts/camera/1_test.py
+++ TestScripts/camera/1_test.py
@@ -5,6 +5,10 @@ from libcamera import Transform
 All the parameters are optional, and default values will be chosen if omitted. The following example will place an
 800x600 pixel preview window at (100, 200) on the display, and will horizontally mirror the camera preview image:
 '''
+# picam2 = Picamera2()
+# picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600, transform=Transform(hflip=1))
+# picam2.start()
+
+
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600, transform=Transform(hflip=1))
-picam2.start()
+picam2.start_preview(Preview.QTGL)
