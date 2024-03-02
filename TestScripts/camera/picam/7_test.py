e420f3ab3e5d7fb9d1125320687ccf4324a14aff
KevMcK <kmcrunk@gmail.com>
2024-03-02 01:19:28 -0600

    test updates

--- TestScripts/camera/picam/7_test.py
+++ TestScripts/camera/picam/7_test.py
@@ -1,9 +1,7 @@
-from io import BytesIO
-from picamera import PiCamera
+import picamera
 
-stream = BytesIO()
-camera = PiCamera()
+camera = picamera.PiCamera()
 camera.resolution = (640, 480)
-camera.start_recording(stream, format='h264', quality=23)
-camera.wait_recording(15)
+camera.start_recording('testvid.h264')
+camera.wait_recording(10)
 camera.stop_recording()
