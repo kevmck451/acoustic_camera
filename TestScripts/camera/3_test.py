b48e747f300f60b8a15f70cb2d2be60f7321635b
KevMcK <kmcrunk@gmail.com>
2024-03-01 23:53:03 -0600

    test updates

--- TestScripts/camera/3_test.py
+++ TestScripts/camera/3_test.py
@@ -1,6 +1,8 @@
 from picamera2 import Picamera2, Preview
 from libcamera import Transform
 
+import time
+
 # Messing with parameters
 # From ch3 in doc
 
@@ -8,3 +10,5 @@ picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600,
 transform=Transform(hflip=1))
 picam2.start()
+
+time.sleep(20)
