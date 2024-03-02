933334a55bea0c33b167cbbc6464cf580031f3d7
KevMcK <kmcrunk@gmail.com>
2024-03-02 02:09:05 -0600

    test updates

--- TestScripts/camera/picam/10_test.py
+++ TestScripts/camera/picam/10_test.py
@@ -2,34 +2,26 @@ import time
 import picamera
 import numpy as np
 
-
-def moving_overlay(number):
-    a = np.zeros((1280, 720, 3), dtype=np.uint8)
-    a[640, :, :] = 0xff
-    a[:, 360, :] = 0xff
-
-    return a
-
-
 # Create an array representing a 1280x720 image of
 # a cross through the center of the display. The shape of
 # the array must be of the form (height, width, color)
-a = np.zeros((1280, 720, 3), dtype=np.uint8)
-a[640, :, :] = 0xff
-a[:, 360, :] = 0xff
+a = np.zeros((720, 1280, 3), dtype=np.uint8)
+a[360, :, :] = 0xff
+a[:, 640, :] = 0xff
 
 camera = picamera.PiCamera()
-camera.rotation = 90
+# camera.rotation = 90
 camera.resolution = (1280, 720)
 camera.framerate = 24
 camera.start_preview()
 # Add the overlay directly into layer 3 with transparency;
 # we can omit the size parameter of add_overlay as the
 # size is the same as the camera's resolution
-o = camera.add_overlay(a.tobytes(), layer=3, alpha=64)
+o = camera.add_overlay(np.getbuffer(a), layer=3, alpha=64)
 try:
     # Wait indefinitely until the user terminates the script
     while True:
         time.sleep(1)
 finally:
     camera.remove_overlay(o)
+
