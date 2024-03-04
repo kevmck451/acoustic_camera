e046e11079cb85c2ca323385677959ee18e57ab6
KevMcK <kmcrunk@gmail.com>
2024-03-04 00:28:21 -0600

    matrix mic investigation

--- TestScripts/mics/recording.py
+++ TestScripts/mics/recording.py
@@ -25,9 +25,9 @@ print("* recording")
 frames = []
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    print(data)
+    # print(data)
     frames.append(data)
-print(frames)
+print(frames[:50])
 print("* done recording")
 
 # kill the mic and recording
