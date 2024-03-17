af849f66c2da4f2c1dd19aeb6d1bf008615ce857
KevMcK <kmcrunk@gmail.com>
2024-03-17 02:25:45 -0500

    audio cube over server

--- TestScripts/acoustic_cam/fpga_socket.py
+++ TestScripts/acoustic_cam/fpga_socket.py
@@ -1,9 +1,4 @@
-
-
-
-import numpy as np
 import socket
-import time
 
 class FPGASocket:
 
@@ -46,57 +41,3 @@ class FPGASocket:
             chunks.append(chunk) # Add to chunks array to build up the message
             bytes_recd = bytes_recd + len(chunk) # Incremenet the number of bytes received
         return b''.join(chunks) # Return a binary representation of the message where all pieces/chunks are joined into one message.
-
-
-
-if __name__ == '__main__':
-    while True:
-        map_row, map_col = 5, 5
-
-        sample_rate = 48000
-        sample_length = 2  # In seconds
-
-        host = '192.168.80.1'  # USB
-        port = 2048
-        N = sample_rate * sample_length
-
-        sock = FPGASocket(map_row * map_col * 2 * N)
-        sock.connect(host, port)
-
-        # Socket set to receive data
-        data = sock.myreceive()
-
-        # frombuffer takes the received data (from socket_test)
-        #   and puts it into an array, viewing the data as 16-bit ints rather than
-        #   unsigned 8-bit ints. The reshape function then takes the created array
-        #   and turns it into a 48000x7x7 data cube (-1 serves as a "fill-in" number
-        #   as the program can tell how the elements divide into a 7x7 size. So, since
-        #   the matrix size is 7x7 (x, 7, 7), it can tell that the total message length
-        #   will be able to fit into 48000 layers, like defined in Line 43. This means
-        #   that 49*2*48000 implicitly chooses the number of matrices).
-        cube = np.frombuffer(data, dtype=np.int16).reshape(-1, map_row, map_col)
-        print(cube.shape)
-
-        # Close socket
-        print("closing")
-        # sock.sock.shutdown(socket.SHUT_RDWR)
-        sock.sock.close()
-        sock = None
-
-        """
-        cube = np.load(f'/Users/noahd/Desktop/EECE4280/Data Cube Samples/Synthetic/PAPA Test Cubes/Test_Cube_{i}.npy')
-        #print(cube)
-        #cube = np.transpose(cube, (2, 0, 1))
-        #cube = cube[:N, :, :]"""
-        print(cube.shape)
-
-
-        # feature_maps = extract_features(cube, map_row, map_col, power_threshold, feature_type[0], feature_params_mfcc, sample_rate, processes)
-
-
-        # time.sleep(1)
-
-        try:
-            pass
-        except KeyboardInterrupt:
-            print("Stream stopped")
