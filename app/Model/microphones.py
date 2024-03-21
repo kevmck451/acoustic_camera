6ceae201c901e83f22807bd6505a0dc13e1495ef
KevMcK <kmcrunk@gmail.com>
2024-03-20 19:56:15 -0500

    init commit

---
+++ app/Model/microphones.py
@@ -0,0 +1,53 @@
+
+
+
+import numpy as np
+
+
+# Local Imports
+from sockets import FPGASocket
+
+
+
+class MicArray:
+    def __init__(self):
+        # this is all specific to the FPGA design
+        self.map_row, self.map_col = 5, 5
+        self.sample_rate = 48000
+        self.sample_length = 0.5  # Half a second
+        self.host = '192.168.80.1'
+        self.port = 2048
+        self.buffer_size = 2 * self.map_row * self.map_col * int(self.sample_rate * self.sample_length)
+        self.sock = None
+
+    def start_client_connection(self):
+        self.sock = FPGASocket(self.host, self.port, self.buffer_size)
+        self.sock.connect()
+
+    def end_client_connection(self):
+        self.sock.close()
+
+    def receive_data_cube(self):
+        N = int(self.sample_rate * self.sample_length)
+        data = self.sock.myreceive()
+        cube = np.frombuffer(data, dtype=np.int16).reshape(-1, self.map_row, self.map_col)
+        return cube
+
+    def get_RMS(self):
+        while True:
+            cube = self.receive_data_cube()
+            rms_values = np.zeros((self.map_row, self.map_col))
+
+            for i in range(self.map_row):
+                for j in range(self.map_col):
+                    square_data = cube[:, i, j]
+                    rms = np.sqrt(np.mean(square_data ** 2))
+                    rms_values[i, j] = rms
+
+            print("RMS values for each square in the cube:")
+            print(rms_values)
+            print('-' * 50)
+
+            return rms_values
+
+
---
+++ app/Model/sockets.py
@@ -0,0 +1,51 @@
+
+
+
+import socket
+
+# Functions to setup things to use the sockets
+# like bash scripts for ssh
+# ssh key-based authentication needs to be set up
+# if start_fpga_server.sh doesnt exist, create it
+
+
+class EventListenerSocket:
+    def __init__(self):
+        pass
+
+
+
+
+class VideoFeedSocket:
+    def __init__(self):
+        pass
+
+
+
+# Socket used for communicating to mics
+class FPGASocket:
+    def __init__(self, host, port, MSGLEN, sock=None):
+        self.host = host
+        self.port = port
+        self.MSGLEN = MSGLEN
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self):
+        self.sock.connect((self.host, self.port))
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < self.MSGLEN:
+            chunk = self.sock.recv(min(self.MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd += len(chunk)
+        return b''.join(chunks)
+
+    def close(self):
+        self.sock.close()
