dbc9675c02e4f85327637286e3d836faf792e800
KevMcK <kmcrunk@gmail.com>
2024-03-20 23:24:02 -0500

    initial set up

--- app/test_main.py
+++ app/test_main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     mic_array = MicArray()
     mic_array.start_client_connection()
 
-    RMS_calculations = threading.Thread(target=mic_array.get_RMS(), daemon=True)
+    RMS_calculations = threading.Thread(target=mic_array.get_RMS, daemon=True)
     RMS_calculations.start()
 
 
