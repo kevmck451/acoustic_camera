f77140e8b0046a98de16ac14114cd701b1f266f7
KevMcK <kmcrunk@gmail.com>
2024-03-20 23:37:31 -0500

    initial set up

--- app/test_main.py
+++ app/test_main.py
@@ -10,16 +10,11 @@ if __name__ == "__main__":
     mic_array = MicArray()
     mic_array.start_client_connection()
 
-    RMS_calculations = threading.Thread(target=mic_array.get_RMS, daemon=True)
+    RMS_calculations = threading.Thread(target=mic_array.get_RMS)
     RMS_calculations.start()
 
 
     while True:
-        try:
-            time.sleep(.1)
-
-        except KeyboardInterrupt:
-            mic_array.end_client_connection()
-
+        time.sleep(.1)
 
 
