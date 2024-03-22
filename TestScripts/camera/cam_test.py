f6ff6c2da0afc497f25893506a34771decf6cb04
KevMcK <kmcrunk@gmail.com>
2024-03-22 02:22:00 -0500

    camera implementation

--- TestScripts/camera/cam_test.py
+++ TestScripts/camera/cam_test.py
@@ -1,19 +1,19 @@
-import cv2
-
-cap = cv2.VideoCapture(0)  # Use the appropriate camera index
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+# import cv2
+#
+# cap = cv2.VideoCapture(0)  # Use the appropriate camera index
+# if not cap.isOpened():
+#     print("Cannot open camera")
+#     exit()
+# while True:
+#     ret, frame = cap.read()
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#         break
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
 
 
 # import cv2
@@ -39,3 +39,20 @@ cv2.destroyAllWindows()
 # update_frame()  # Start the frame update loop
 #
 # root.mainloop()
+
+
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+ret, frame = cap.read()
+if ret:
+    cv2.imshow('Camera', frame)
+    cv2.waitKey(0)
+else:
+    print("Can't receive frame (stream end?). Exiting ...")
+cap.release()
+cv2.destroyAllWindows()
