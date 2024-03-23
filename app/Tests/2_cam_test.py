0c039d71fac59fd8e8a0cc6afde690f9327069e4
KevMcK <kmcrunk@gmail.com>
2024-03-22 19:43:00 -0500

    camera implementation

--- app/Tests/2_cam_test.py
+++ app/Tests/2_cam_test.py
@@ -292,8 +292,10 @@ if __name__ == "__main__":
     camera = BufferlessVideoCapture(color=True, skip_frames=1)
     while True:
         # print(type(camera.latest_frame))
-        if type(camera.latest_frame) == 'numpy.ndarray':
+        try:
             print(camera.latest_frame.shape)
             print(camera.latest_frame.dtype)
+        except:
+            pass
         # print(camera.latest_frame)
     # camera.start_viewing()
