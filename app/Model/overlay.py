0d0d3d7f884d92906f11d216de9381ab14b534d9
KevMcK <kmcrunk@gmail.com>
2024-03-24 01:27:54 -0500

    app redesign

--- app/Model/overlay.py
+++ app/Model/overlay.py
@@ -149,7 +149,7 @@ class Overlay:
                 # Calculate the number of bytes: 921600 bytes
                 # num_bytes = self.total_overlay_compressed.nbytes
                 # print(num_bytes)
-
+                print(self.stream_video)
                 if self.stream_video:
                     self.video_client.send_data(self.total_overlay_compressed)
                     print('frame sent')
