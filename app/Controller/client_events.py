bad5873a19f7e1c34f146212468c1533b065885f
KevMcK <kmcrunk@gmail.com>
2024-03-24 00:05:14 -0500

    app redesign

--- app/Controller/client_events.py
+++ app/Controller/client_events.py
@@ -27,8 +27,8 @@ class Event_Sender_Client:
                 response = self.socket.recv(1024)
                 if not response.decode('utf-8') == 'ack': continue
                 print(f"Connected to {self.host}:{self.port}")
-                # self.heartbeat_thread = threading.Thread(target=self.heartbeat, daemon=True)
-                # self.heartbeat_thread.start()
+                self.heartbeat_thread = threading.Thread(target=self.heartbeat, daemon=True)
+                self.heartbeat_thread.start()
                 self.connected = True
 
             except Exception as e:
