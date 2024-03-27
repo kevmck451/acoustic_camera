ad5c2e6d9cd39b4228eeac4cba21d6f63ea96ccb
KevMcK <kmcrunk@gmail.com>
2024-03-26 22:50:33 -0500

    video server client testing

--- app/Model/server_video.py
+++ app/Model/server_video.py
@@ -1,4 +1,6 @@
 
+# First Draft
+
 from dataclasses import dataclass
 import threading
 import socket
@@ -11,9 +13,9 @@ import time
 # FPGA / Mics giving data | RP / Video giving data
 
 class Video_Server:
-    def __init__(self, host='0.0.0.0', port=56565):
-        self.host = host
-        self.port = port
+    def __init__(self, host='0.0.0.0'):
+        self.host = host # self._get_ip()
+        self.port = 56565
         print(f"listening at IP {self.host} port {self.port}")
 
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,65 +23,78 @@ class Video_Server:
         self.server_socket.listen(1)
 
         self.video_hw = None
-        self.send_video_stream = False  # Initially set to False
-
-    def run(self):
-        while True:
-            client_socket, address = self.server_socket.accept()
-            print(f"Client connected from {address}")
-            # Handle each client connection in a separate thread
-            threading.Thread(target=self.capture, args=(client_socket,), daemon=True).start()
-
-    def handle_client(self, client_socket):
-        try:
-            while self.send_video_stream:
-                data_bytes = self.video_hw.get_data()
-                if data_bytes:
-                    client_socket.sendall(data_bytes)
-        except Exception as e:
-            print(f"Error during video capture: {e}")
-        finally:
-            client_socket.close()
-
-    def capture(self, sock):
-        while self.send_video_stream:
-            try:
-                data_bytes = self.video_hw.get_data()
-                if not data_bytes:
-                    print("No data received, waiting...")
-                    time.sleep(0.1)  # Briefly pause to avoid overwhelming the CPU
-                    continue
-
-                print(f"Sending {len(data_bytes)} bytes of video data")
-
-                # Send the data in chunks to the client
-                bytes_sent = 0
-                while bytes_sent < len(data_bytes):
-                    sent = sock.send(data_bytes[bytes_sent:bytes_sent + 4096])
-                    if sent == 0:
-                        print("Connection probably broken")
-                        return  # Exit the function if connection is broken
-                    bytes_sent += sent
-
-            except Exception as e:
-                print(f"Error during video capture: {e}")
-                break  # Break the loop in case of an error
-
-    # Additional methods for start and stop commands
-    def start_streaming(self):
-        print('Streaming Video!')
         self.send_video_stream = True
 
-    def stop_streaming(self):
-        print('Stopped Streaming Video!')
-        self.send_video_stream = False
+
+    @staticmethod
+    def _get_ip():
+        # https://stackoverflow.com/a/28950776
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.254.254.254', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
+
+
+    def run(self):
+        try:
+            while True:
+                (client_socket, address) = self.server_socket.accept()
+                try:
+                    self.capture(client_socket)
+                    print("client said goodbye")
+                except (ConnectionResetError, ConnectionAbortedError,
+                        BrokenPipeError):
+                    print("client left rudely")
+                finally:
+                    client_socket.close()
+        finally:
+            self.server_socket.close()
 
 
     def set_video_hw(self, video_hw):
         self.video_hw = video_hw
 
+
+    def capture(self, sock):
+        print("Video Stream is Ready!")
+
+        while True:
+            try:
+                if self.send_video_stream:
+                    data_bytes = self.video_hw.get_data()
+                    if not data_bytes:
+                        print("No data received, waiting...")
+                        time.sleep(0.1)
+                        continue
+
+                    print(f"Sending {len(data_bytes)} bytes of video data")
+
+                    # Send the data in chunks to the client
+                    bytes_sent = 0
+                    while bytes_sent < len(data_bytes):
+                        sent = sock.send(data_bytes[bytes_sent:bytes_sent + 4096])
+                        if sent == 0:
+                            print("Connection probably broken")
+                            return
+                        bytes_sent += sent
+
+            except Exception as e:
+                print(f"Error during video capture: {e}")
+                break
+
+            time.sleep(0.1)  # Give some delay to prevent overloading the network
+
+        print("Video capture ended")
+
+
     def stop(self):
-        self.send_video_stream = False
         self.server_socket.close()
 
 # To run the server
@@ -90,115 +105,3 @@ if __name__ == '__main__':
 
     while True:
         time.sleep(0.1)
-
-
-
-
-
-
-# First Draft
-
-# from dataclasses import dataclass
-# import threading
-# import socket
-# import time
-#
-
-# ----------------------------------------------------
-# Server Code
-# ----------------------------------------------------
-# FPGA / Mics giving data | RP / Video giving data
-
-# class Video_Server:
-#     def __init__(self, host='0.0.0.0'):
-#         self.host = host # self._get_ip()
-#         self.port = 56565
-#         print(f"listening at IP {self.host} port {self.port}")
-#
-#         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self.server_socket.bind((self.host, self.port))
-#         self.server_socket.listen(1)
-#
-#         self.video_hw = None
-#         self.send_video_stream = True
-#
-#
-#     @staticmethod
-#     def _get_ip():
-#         # https://stackoverflow.com/a/28950776
-#         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#         s.settimeout(0)
-#         try:
-#             # doesn't even have to be reachable
-#             s.connect(('10.254.254.254', 1))
-#             IP = s.getsockname()[0]
-#         except Exception:
-#             IP = '127.0.0.1'
-#         finally:
-#             s.close()
-#         return IP
-#
-#
-#     def run(self):
-#         try:
-#             while True:
-#                 (client_socket, address) = self.server_socket.accept()
-#                 try:
-#                     self.capture(client_socket)
-#                     print("client said goodbye")
-#                 except (ConnectionResetError, ConnectionAbortedError,
-#                         BrokenPipeError):
-#                     print("client left rudely")
-#                 finally:
-#                     client_socket.close()
-#         finally:
-#             self.server_socket.close()
-#
-#
-#     def set_video_hw(self, video_hw):
-#         self.video_hw = video_hw
-#
-#
-#     def capture(self, sock):
-#         print("Video Stream is Ready!")
-#
-#         while True:
-#             try:
-#                 if self.send_video_stream:
-#                     data_bytes = self.video_hw.get_data()
-#                     if not data_bytes:
-#                         print("No data received, waiting...")
-#                         time.sleep(0.1)
-#                         continue
-#
-#                     print(f"Sending {len(data_bytes)} bytes of video data")
-#
-#                     # Send the data in chunks to the client
-#                     bytes_sent = 0
-#                     while bytes_sent < len(data_bytes):
-#                         sent = sock.send(data_bytes[bytes_sent:bytes_sent + 4096])
-#                         if sent == 0:
-#                             print("Connection probably broken")
-#                             return
-#                         bytes_sent += sent
-#
-#             except Exception as e:
-#                 print(f"Error during video capture: {e}")
-#                 break
-#
-#             time.sleep(0.1)  # Give some delay to prevent overloading the network
-#
-#         print("Video capture ended")
-#
-#
-#     def stop(self):
-#         self.server_socket.close()
-#
-# # To run the server
-# if __name__ == '__main__':
-#     server = Video_Server('0.0.0.0')
-#     server.send_video_stream = True
-#     run_thread = threading.Thread(target=server.run, daemon=True).start()
-#
-#     while True:
-#         time.sleep(0.1)
